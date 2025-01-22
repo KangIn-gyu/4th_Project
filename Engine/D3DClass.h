d0878a0ba44bdaa600eea42dcef14b431bbb2bcf
Sina Ham <lihena8@gmail.com>
2025-01-22 21:35:00 +0900

    D2D폰트 추가하려했으나 D2D1Device 초기화에 실패

--- Engine/D3DClass.h
+++ Engine/D3DClass.h
@@ -1,9 +1,6 @@
 #pragma once
 #include <d3d11.h>
 #include <dxgi.h>
-#include <d3dcommon.h>
-#include <directxtk/SimpleMath.h>
-
 #include "Viewport.h"
 
 struct WindowInfo;
@@ -23,27 +20,33 @@ public:
 	// (캡슐화 포기) 이건 그냥 주자 구조만들기 너무 힘듬
 	static ComPtr<ID3D11Device> GetD3DDevice() { return D3DDevice; }
 	static ComPtr<ID3D11DeviceContext> GetD3DDeviceContext() { return D3DDeviceContext; }
-
-	std::unique_ptr<Viewport>& GetVieport() { return viewport; }
+	static ComPtr<ID2D1SolidColorBrush> GetD2DBrush() { return Brush; }
+	static ComPtr<ID2D1DeviceContext> GetD2DDeviceContext() { return D2DDeviceContext; }
+	static std::unique_ptr<Viewport>& GetViewport() { return viewport; }
 
 	void CreateSamplerState(D3D11_FILTER _filter, D3D11_TEXTURE_ADDRESS_MODE _addressMode, ComPtr<ID3D11SamplerState> _sampler);
 	std::pair<int, int> GetWindowsSize();
+	void CreateD2DRenderTarget();
 
 	void ExtractFinalImage(); //  프론트 버퍼 렌더링
 	ComPtr<ID3D11ShaderResourceView> GetImGuiImageTexture() { return SRV; }
 private:
 	void InitD3D();
 	void InitDXGI();
+	void InitD2D();
 
 	DXGI_SWAP_CHAIN_DESC CreateSwapDesc();
 	void CreateDepthStencilBuffer();
 
-
 public:
 
 private:
-	static ComPtr<ID3D11Device>        D3DDevice;           // 디바이스	                      
-	static ComPtr<ID3D11DeviceContext> D3DDeviceContext;    // 즉시 디바이스 컨텍스트
+	static ComPtr<ID3D11Device>				D3DDevice;           // 디바이스	                      
+	static ComPtr<ID3D11DeviceContext>		D3DDeviceContext;    // 즉시 디바이스 컨텍스트
+	static std::unique_ptr<Viewport>		viewport;
+	static ComPtr <ID2D1SolidColorBrush>	Brush;
+	static ComPtr<ID2D1DeviceContext>		D2DDeviceContext;
+
 	ComPtr<IDXGISwapChain>		       swapChain;           // 스왑체인
 
 	ComPtr<ID3D11RenderTargetView>	   renderTargetView;    // 렌더링 타겟뷰
@@ -54,15 +57,20 @@ private:
 	ComPtr<ID3D11BlendState>		   alphaBlendState;
 
 	WindowInfo* windowInfo;
-	std::unique_ptr<Viewport> viewport;
+
 	bool presentEnabled;
 
 	ComPtr<IDXGIFactory> DXGIFactory;
 	ComPtr<IDXGIAdapter> DXGIAdapter;
 	ComPtr<IDXGIDevice>  DXGIDevice;
+	ComPtr<IDXGISurface>  DXGISurface;	// Direct3D와 Direct2D 간의 데이터 교환을 가능하게 해줌
 
 	ComPtr<ID3D11Texture2D> stagingTexture; // ImGui에 넘길 텍스쳐
 	ComPtr<ID3D11ShaderResourceView> SRV;
+
+	ComPtr<ID2D1Device>			D2DDevice = nullptr;
+	ComPtr<ID2D1Factory1>		D2DFactory1 = nullptr;
+	ComPtr<ID2D1Bitmap1>		D2DBitmap1 = nullptr;
 };
 
 // 항상 그래픽 파이프 라인을 생각하자
