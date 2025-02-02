378040feebfb5923fc2e2e2f5fc2ebd3e38c85eb
matstar33 <76931377+matstar33@users.noreply.github.com>
2025-02-02 21:49:01 +0900

    Revert "Merge branch 'gyu'"
    This reverts commit 5cc9e1a511d4c4063a96c74296b6882a2aa61f36, reversing
    changes made to 544b406b9ffdba8da83ff1a588d0a6c1b65684e3.

--- KangIngyu/KangScene.cpp
+++ KangIngyu/KangScene.cpp
@@ -2,7 +2,6 @@
 #include "KangScene.h"
 #include "AnimationObj.h"
 #include "BaseObj.h"
-#include "../Engine/SoundSystem.h"
 
 KangScene::KangScene(std::string_view _Name) : Scene(_Name)
 {
@@ -11,7 +10,6 @@ KangScene::KangScene(std::string_view _Name) : Scene(_Name)
 
 void KangScene::Enter()
 {
-	SOUNDSYSTEM->PlayMusic(eSoundList::Main_Theme, eSoundChannel::BGM);
 	CreatorObject<AnimationObj>("AnimationObj", Object::ObjectType::Basic);
 //	CreatorObject<BaseObj>("BaseObj", Object::ObjectType::Basic);
 }
