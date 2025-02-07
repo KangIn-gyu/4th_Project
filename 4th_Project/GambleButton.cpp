ed999fdb42c3802c4922e831cb1ab6bc334b0667
SongSeHwan <sshhss9900@gmail.com>
2025-02-07 10:31:51 +0900

    12312

---
+++ 4th_Project/GambleButton.cpp
@@ -0,0 +1,51 @@
+#include "pch.h"
+#include "GambleButton.h"
+
+#include "../Engine/D2DRenderComponent.h"
+#include "../Engine/TransformComponent.h"
+#include "../Engine/ButtonColider.h"
+#include "../Engine/BoxCollider.h"
+
+GambleButton::GambleButton(std::string_view _name, Object::ObjectType _type, std::string_view _filePath, std::function<void()> _func) :Object(_name, _type)
+{
+	clickFunc = _func;
+	imageFilepath = _filePath;
+	imagedata = CreateComponent<D2DRenderComponent>();
+}
+
+GambleButton::GambleButton(std::string_view _name, Object::ObjectType _type, std::string_view _filePath, DXMath::Vector2 _pos, std::function<void()> _func) :Object(_name, _type)
+{
+	pos = _pos;
+	clickFunc = _func;
+	imageFilepath = _filePath;
+	imagedata = CreateComponent<D2DRenderComponent>();
+}
+
+GambleButton::~GambleButton()
+{
+}
+
+void GambleButton::Initialize()
+{
+	Object::Initialize();
+	imagedata->Load2DImage(imageFilepath);
+	//GetComponent<ButtonColider>()->SetBoundBox(0, 0, { imagedata->Get2DImageSize().x,imagedata->Get2DImageSize().y,0 });
+	imagedata->Set2DImagePos(pos.x, pos.y);
+	CreateComponent<BoxCollider>();
+	auto xy = imagedata->Get2DImageXY();
+	DXMath::Vector3 center = { pos.x + xy.x / 2,  pos.y + xy.y / 2,    0.f };
+	std::cout << imagedata->Get2DImageSize().x << " dawdawd" << imagedata->Get2DImageSize().y << std::endl;
+	DXMath::Vector3 extent = { xy.x / 2 ,xy.y / 2 , 0.1f };
+	GetComponent<BoxCollider>()->SetBox(center, extent, DXMath::Quaternion::Quaternion(0, 0, 0, 1));
+}
+
+void GambleButton::Update(const float _deltaTime)
+{
+	Object::Update(_deltaTime);
+}
+
+void GambleButton::OnClick()
+{
+	std::cout << "2d´­·¶À½ " << std::endl;
+	clickFunc();
+}
