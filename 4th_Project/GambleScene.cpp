9175b76413916a8867aaf177e540b429b58cfe84
Kaningyu <exia0015@naver.com>
2025-02-07 10:49:59 +0900

    함수 몇개 이름 변경 안되서 다시 로드

--- 4th_Project/GambleScene.cpp
+++ 4th_Project/GambleScene.cpp
@@ -26,11 +26,11 @@ void GambleScene::Enter()
 	GetGameObject(Object::ObjectType::Camera)->GetComponent<TransformComponent>()->SetPosition({ -30.0f, 130.0f, -83.0f });
 	GetGameObject(Object::ObjectType::Camera)->GetComponent<TransformComponent>()->SetQuaternion(DXMath::Quaternion::Quaternion(0.3f, 0.171f, -0.059f, 0.93f));
 
-	BLACKJACK->dealer = ObjectCreator<Dealer>("Dealer", Object::ObjectType::Basic); 
+	BLACKJACK->dealer = CreatorObject<Dealer>("Dealer", Object::ObjectType::Basic);
 	BLACKJACK->dealer->GetComponent<TransformComponent>()->SetPosition({ 00, -13.0f, 50.0f });
-	BLACKJACK->deck = ObjectCreator<Deck>("Deck", Object::ObjectType::Basic);
+	BLACKJACK->deck = CreatorObject<Deck>("Deck", Object::ObjectType::Basic);
 	BLACKJACK->deck->GetComponent<TransformComponent>()->SetPosition({ -60, 65, 0 });
-	BLACKJACK->trashDeck = ObjectCreator<Deck>("trashDeck", Object::ObjectType::Basic,false);
+	BLACKJACK->trashDeck = CreatorObject<Deck>("trashDeck", Object::ObjectType::Basic,false);
 
 	BLACKJACK->Setstage(1);
 	CreatorObject<TestObj>("Table", Object::ObjectType::Basic);
